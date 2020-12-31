ccec63de2895a0a2bebb9560a30618b507cc589e
wizif <rezagina68@gmail.com>
2020-12-31 19:54:03 +0330

    buy book buttons works better
    this button now can recoginize is products free or permium and show download or shopping button

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,30 +1,47 @@
 import { Button } from '@material-ui/core';
-import { AddShoppingCartOutlined } from '@material-ui/icons';
+import { AddShoppingCartOutlined, ArrowDownward } from '@material-ui/icons';
 import React from 'react';
 import { myStyles } from '../global/gbvars';
 export function BuyButton(props){
     const bookInfo=props.book;
     console.log(bookInfo)
     const classes=myStyles();
-    return(
-        <Button
-        color='secondary'
-        onClick={()=>{
-            if(localStorage['checkout']){
-                let newArr=JSON.parse(localStorage['checkout'])
-                newArr.push(bookInfo)
-                localStorage['checkout']=JSON.stringify(newArr)
-                
-            }
-            else{
-                localStorage.setItem('checkout',JSON.stringify([]))
-                let newArr=JSON.parse(localStorage['checkout'])
-                newArr.push(bookInfo)
-                localStorage['checkout']=JSON.stringify(newArr)
-            }
-        }}
-        >
-            <AddShoppingCartOutlined/>
-        </Button>
-    )
+    if(props.book.price === 'free'){
+        return(
+            <Button 
+                onClick={()=>window.open(props.book.url,'_blank')}
+                className={classes.boldFonts} 
+                variant='outlined' 
+                color='primary'>
+                        <ArrowDownward/>
+                </Button>
+        )
+    }
+    else{
+        return(
+            <Button
+            color='secondary'
+            variant='outlined'
+            className={classes.warningFonts}
+            onClick={()=>{
+                if(localStorage['checkout']){
+                    let newArr=JSON.parse(localStorage['checkout'])
+                    newArr.push(bookInfo)
+                    localStorage['checkout']=JSON.stringify(newArr)
+                    
+                }
+                else{
+                    localStorage.setItem('checkout',JSON.stringify([]))
+                    let newArr=JSON.parse(localStorage['checkout'])
+                    newArr.push(bookInfo)
+                    localStorage['checkout']=JSON.stringify(newArr)
+                }
+            }}
+            >
+                <AddShoppingCartOutlined/>
+                تومان{props.book.price}
+            </Button>
+        )
+    }
+    
 }
