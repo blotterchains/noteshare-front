7add0268d383ecea03ccaccedbc420b7d5d9453d
wizif <rezagina68@gmail.com>
2020-12-31 19:57:08 +0330

    changes in inorder
    like checking localstorage and a new grid to show sum of the product price

--- src/screens/inOrder.jsx
+++ src/screens/inOrder.jsx
@@ -1,10 +1,19 @@
-import { Box, Grid } from '@material-ui/core';
+import { Box, Button, Divider, Grid } from '@material-ui/core';
+import { Delete } from '@material-ui/icons';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { borderRadius, myStyles } from '../global/gbvars';
 function RenderItems(props){
-    const orders=JSON.parse(localStorage['checkout'])
     const classes=myStyles()
+    console.log(props)
+    const orders=props.orders;
+    const setOrders=props.setOrders;
+    const handleDelete=(index)=>{
+        let order=JSON.parse(localStorage['checkout'])
+        order.splice(index,1)
+        localStorage['checkout']=JSON.stringify(order)
+        setOrders(order)
+    }
     if(orders.length===0){
         return('چیزی در سبد خرید موجود نیست')
     }else{
@@ -16,7 +25,7 @@ function RenderItems(props){
                     className={classes.innerinnerRoot}
                     borderRadius='3vmin'
                     boxShadow={3}
-                    style={{width:'50%'}}
+                    
                     >
                         <Grid 
                         spacing={3}
@@ -32,8 +41,17 @@ function RenderItems(props){
                                     height:'30vh',
                                     width:'8vw'}} 
                                 />
+                                
+                                        <Button 
+                                        variant='outlined'
+                                        color='secondary'
+                                        onClick={()=>{handleDelete(index)}}
+                                        >
+                                            <Delete/>
+                                        </Button>
+                                    
                             </Grid>
-                            <Grid style={{width:'40%'}} item>
+                            <Grid item>
                            
                                 <Grid container>
                                     <Grid item>
@@ -83,6 +101,18 @@ function RenderItems(props){
                                         </p>
                                     </Grid>
                                 </Grid>
+                                <Grid container>
+                                    <Grid item>
+                                        <p className={classes.boldFonts}>
+                                            قیمت:
+                                        </p>
+                                    </Grid>
+                                    <Grid item>
+                                        <p className={classes.descriptionFonts}>
+                                            {item.price}
+                                        </p>
+                                    </Grid>
+                                </Grid>
                                 
                         </Grid>
                         </Grid>
@@ -93,15 +123,35 @@ function RenderItems(props){
     }
 
 }
+function CheckOut(props){
+    let price=0;
+    let counts=0
+    props.orders.map((item,index)=>{
+           price+=item.price;
+           counts=index
+    })
+    return(
+       <div>
+           مبلغ قابل پرداخت:{price}
+           <br/>
+           تعداد:{counts+1}
+       </div>
+
+    )
+}
 export function InOrder(props){
     const classes = myStyles();
     const history=useHistory();
+    const [orders,setOrders]=React.useState(JSON.parse(localStorage['checkout']))
+
     if(localStorage['usertoken']){
         if(localStorage['checkout']){
+
             return(
                 <div
                 className={classes.root}
                 >
+                    
                     <Box
                     boxShadow={3}
                     borderRadius={borderRadius}
@@ -111,11 +161,36 @@ export function InOrder(props){
                         <p className={classes.boldFonts}>
                             سبد خرید شما
                         </p>
-                        <div dir='rtl'>
-                            <RenderItems/>
-                        </div>
                         
+                        <Grid dir='rtl' container>
+                            <Grid style={{width:'50%'}} item>
+                            <div dir='rtl'>
+                                <RenderItems
+                                setOrders={setOrders} orders={orders}
+                                />
+                            </div>
+                            </Grid>
+                            <Grid style={{width:'30%'}} item>
+                                <Box
+                                borderRadius={borderRadius}
+                                boxShadow={3}
+                                style={{width:'100%'}}
+                                className={classes.innerinnerRoot}
+                                >
+                                    <CheckOut orders={orders}/>
+                                    <Button 
+                                    onClick={()=>history.push('/buy')}
+                                    color='secondary'>
+                                        پرداخت
+                                    </Button>
+                                </Box>
+                                
+                            </Grid>
+                        </Grid>
+                        
+                    
                     </Box>
+                    
                 </div>
             )
         }else{
@@ -135,9 +210,11 @@ export function InOrder(props){
                         </p>
                         <div dir='rtl'>
                             <RenderItems/>
+                            
                         </div>
                         
                     </Box>
+                   
                 </div>
             )
         }
