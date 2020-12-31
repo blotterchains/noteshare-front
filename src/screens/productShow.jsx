8e40f1e371aeb51c2a8e8625a4bb25cda59f0f47
wizif <rezagina68@gmail.com>
2020-12-31 05:07:43 +0330

    changes in product show
    product show changed and comments added to this page.and several styles changed coz comments added

--- src/screens/productShow.jsx
+++ src/screens/productShow.jsx
@@ -1,11 +1,58 @@
-import { Box, Grid } from '@material-ui/core';
-import { NearMeSharp } from '@material-ui/icons';
+import { Box, Button, Divider, Grid, TextField } from '@material-ui/core';
+import { AddShoppingCartOutlined, CommentOutlined, NearMeSharp, SendOutlined } from '@material-ui/icons';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { BuyButton } from '../components/buttons';
 import { SlideShow } from '../components/slideshow';
 import { fetchapiWithotToken, myStyles,borderRadius } from '../global/gbvars';
+function RenderComments(props){
+    const classes=myStyles()
+    if(props.comments && props.comments.length!==0){
+        return(
+            props.comments.map((item,index)=>{
+                console.log(item)
+                return(
+                    <div>
+                         <Box
+                            borderRadius={borderRadius}
+                            boxShadow={3}
+                            className={classes.innerRoot}
+                            >
+                                <Grid 
+                                spacing={3}
+                                dir='rtl' container>
+                                    <Grid item>
+                                        <p className={classes.boldFonts}>
+                                            {item.username}
+                                        </p>
+                                    </Grid>
+                                    <Grid item>
+                                        <p className={classes.descriptionFonts}>
+                                            {item.date}
+                                        </p>
+                                    </Grid>
+                                </Grid>
+                                <div style={{alignItems:'right'}}>
+                                    <p className={classes.descriptionFonts}>
+                                        {item.text}
+                                    </p>
+                                </div>
+                                
+                            </Box>
+                    </div>
+                )
+            })
+        )
+    }
+    else{
+        return 'هیچ کامنتی وجود ندارد'
+    }
+    
+}
 export function ProductShow(props){
     const hash=props.match.params.hash;
     const username=props.match.params.username;
+    const history=useHistory()
     const classes=myStyles();
     const [author,setAuthor]=React.useState("");
     const [description,setDescription]=React.useState("");    
@@ -16,7 +63,11 @@ export function ProductShow(props){
     const [type,setType]=React.useState('نوع محصول')
     const [images,setImages]=React.useState([]);
     const [url,setUrl]=React.useState('');
-    React.useEffect(() => {
+    const [comments,setComments]=React.useState([]);
+    const [bookInfo,setBookInfo]=React.useState({});
+    let comText='';
+    
+    const getBooksInfo=()=>{
         fetchapiWithotToken({hash:hash,username:username},'books-info')
         .then((response)=>{
             setAuthor(response.author);
@@ -27,7 +78,27 @@ export function ProductShow(props){
             setType(response.type);
             setImages(response.images);
             setUrl(response.url);
+            setComments(response.comments);
+            setBookInfo(response)
+
         })
+    }
+    const handleSendComments=()=>{
+        if(localStorage['usertoken']){
+            fetchapiWithotToken({
+                hash:hash,
+                username:username,
+                text:comText,
+                token:localStorage['usertoken']
+            },'books-comment')
+            .then((response)=>getBooksInfo())
+        }else{
+            alert('maluciouse activity')
+        }
+        
+    }
+    React.useEffect(() => {
+        getBooksInfo()
         return () => {
             
         }
@@ -47,8 +118,23 @@ export function ProductShow(props){
                         <SlideShow 
                         disNavButton
                         images={images}/>
+                        {
+                            price==='free' ? (
+                                <Button
+                                onClick={()=>window.open(url,'_blank')}
+                                >
+                                    دانلود
+                                </Button>
+                            ):(
+                            <BuyButton
+                            book={bookInfo}
+                            />
+                            
+                            )
+                        }
+                        
                     </Grid>
-                    <Grid style={{width:'60%'}} item>
+                    <Grid style={{width:'40%'}} item>
                         <Box 
                         boxShadow={3}
                         borderRadius={borderRadius}
@@ -111,41 +197,79 @@ export function ProductShow(props){
                                 
                             </Grid>):('')}
                             <Grid container>
-                                <Grid item>
+                                <Grid style={{width:'20%'}} item>
                                     <p className={classes.boldFonts}>
                                         توضیحات:
                                     </p>
                                 </Grid>
-                                <Grid item>
-                                    <p className={classes.descriptionFonts}>
+                                <Grid style={{maxWidth:'80%'}} item>
+                                    <p 
+                                    style={{width:'70%'}}
+                                    className={classes.descriptionFonts}>
                                         {description}
                                     </p>
                                 </Grid>
                             </Grid>
-                            <Grid container>
-                                <Grid item>
-                                    <p className={classes.boldFonts}>
-
-                                    </p>
-                                </Grid>
-                                <Grid item>
-                                    <p className={classes.descriptionFonts}>
-
-                                    </p>
-                                </Grid>
-                            </Grid>
-                            <Grid container>
-                                <Grid item>
-                                    <p className={classes.boldFonts}>
-
-                                    </p>
-                                </Grid>
-                                <Grid item>
-                                    <p className={classes.descriptionFonts}>
-
-                                    </p>
-                                </Grid>
-                            </Grid>
+                           
+                        </Box>
+                    </Grid>
+                    <Grid 
+                    style={{width:'40%'}}
+                    item>
+                        <Box 
+                        borderRadius={borderRadius}
+                        boxShadow={3}
+                        style={{height: '80%'}}
+                        className={classes.innerinnerRoot}>
+                            <p className={classes.boldFonts}>
+                            comments
+                            </p>
+                            
+                            <Box
+                            borderRadius={borderRadius}
+                            boxShadow={3}
+                            style={{
+                                overflowY:'scroll',
+                                height:'80%'}}
+                            >
+                                
+                               <RenderComments comments={comments}/>
+                               
+                                
+                            </Box>
+                            {
+                                 localStorage['userToken'] ? (
+                                         <Grid container>
+                                             <Grid style={{width:'70%'}} item>
+                                                <TextField 
+                                                    variant='filled' 
+                                                    placeholder='نظر شما'
+                                                    multiline
+                                                    row={3}
+                                                    onChange={(e)=>comText=e.target.value}
+                                                    />
+                                                
+                                             </Grid>
+                                             <Grid style={{width:'30%'}} item>
+                                             <Button
+                                                onClick={()=>handleSendComments()}
+                                                >
+                                                    <CommentOutlined/>
+                                                </Button>
+                                             </Grid>
+                                         </Grid>                                    
+                                ):(
+                                    
+                                        <Button
+                                        onClick={()=>{
+                                            history.push('/login')
+                                        }}
+                                        >
+                                        شما باید وارد شوید
+                                    </Button>
+                                )
+                            }
+                            
                         </Box>
                     </Grid>
                 </Grid>
