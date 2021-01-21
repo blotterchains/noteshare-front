631a20ebdafa766ba29b2bd56e69d7b659c55469
wizif <rezagina68@gmail.com>
2021-01-21 17:30:31 +0330

    changes in buybutton
    this component now have a new condition that props.buyer must send as property
    and global context added to this component that send from creatContext

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -2,11 +2,12 @@ import { Button } from '@material-ui/core';
 import { AddShoppingCartOutlined, ArrowDownward } from '@material-ui/icons';
 import React from 'react';
 import { myStyles } from '../global/gbvars';
+import { States } from './states';
 export function BuyButton(props){
     const bookInfo=props.book;
     console.log(bookInfo)
     const classes=myStyles();
-    if(props.book.price === 'free'){
+    if(props.book.price === 'free' || props.buyer){
         return(
             <Button 
                 onClick={()=>window.open(props.book.url,'_blank')}
@@ -19,28 +20,37 @@ export function BuyButton(props){
     }
     else{
         return(
-            <Button
-            color='secondary'
-            variant='outlined'
-            className={classes.warningFonts}
-            onClick={()=>{
-                if(localStorage['checkout']){
-                    let newArr=JSON.parse(localStorage['checkout'])
-                    newArr.push(bookInfo)
-                    localStorage['checkout']=JSON.stringify(newArr)
-                    
+            <States.Consumer>
+                {
+                 states=>{
+                     return(
+                    <Button
+                color='secondary'
+                variant='outlined'
+                className={classes.warningFonts}
+                onClick={()=>{
+                    if(localStorage['checkout']){
+                        let newArr=JSON.parse(localStorage['checkout'])
+                        newArr.push(bookInfo)
+                        localStorage['checkout']=JSON.stringify(newArr)
+                        
+                    }
+                    else{
+                        localStorage.setItem('checkout',JSON.stringify([]))
+                        let newArr=JSON.parse(localStorage['checkout'])
+                        newArr.push(bookInfo)
+                        localStorage['checkout']=JSON.stringify(newArr)
+                    }
+                }}
+                >
+                    <AddShoppingCartOutlined/>
+                    تومان{props.book.price}
+                </Button>
+                     )
+                 }   
                 }
-                else{
-                    localStorage.setItem('checkout',JSON.stringify([]))
-                    let newArr=JSON.parse(localStorage['checkout'])
-                    newArr.push(bookInfo)
-                    localStorage['checkout']=JSON.stringify(newArr)
-                }
-            }}
-            >
-                <AddShoppingCartOutlined/>
-                تومان{props.book.price}
-            </Button>
+            </States.Consumer>
+            
         )
     }
     
