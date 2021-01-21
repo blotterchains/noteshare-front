eb706fc8ff33e2b8cdbb2999e3120abb9df9d539
wizif <rezagina68@gmail.com>
2021-01-21 17:33:10 +0330

    several route added
    allvcbooks and notes added and a new route to show payment is success or unsuccess,after simulate the payment getway(that we produce in backend with python)then we can check with this route and show the users

--- src/components/routes.js
+++ src/components/routes.js
@@ -9,6 +9,11 @@ import {UserPanel} from '../screens/userpanel.jsx';
 import TopNav, { TopNavRight } from './topnav';
 import { ProductShow } from '../screens/productShow.jsx';
 import { InOrder } from '../screens/inOrder.jsx';
+import { AllVCBook } from '../screens/allvcbook.jsx';
+import { AllNotes } from '../screens/allnotes.jsx';
+import { States } from './states.js';
+import { Buy } from '../screens/buy.jsx';
+import { myStyles } from '../global/gbvars.js';
 const useStyles=makeStyles({
     root:{
         width: '50%'
@@ -28,6 +33,14 @@ const screens=[
         render:<AllBook/>
     },
     {
+        path:'/vcbook',
+        render:<AllVCBook/>
+    },
+    {
+        path:'/note',
+        render:<AllNotes/>
+    },
+    {
         path:'/login',
         render:<Login/>
     },
@@ -38,12 +51,21 @@ const screens=[
     {
         path:'/order',
         render:<InOrder/>
-    }
+    },
 ]
 export function Routes(){
     const classes=useStyles();
+    const classes1=myStyles();
+    const [orders,setOrders]=React.useState(JSON.parse(localStorage['checkout']));
     function HandleRoutes(){
-        return(screens.map((i,k)=><Route exact path={i.path} render={(props)=>i.render}></Route>))
+        return(screens.map((i,k)=><Route exact path={i.path} render={(props)=>{
+            return(
+                
+            i.render
+            
+                
+            )
+            }}></Route>))
         
     }
     return(
@@ -52,13 +74,18 @@ export function Routes(){
             <TopNav/>
                 
                 <Divider />
-                <HandleRoutes/>
+                
+                 <HandleRoutes/>
+               
                 <Route exact path='/:hash/active' render={(props)=><ActiveNewAcc {...props}/>}></Route>
                 <Route exact path='/:username/:hash/book' render={(props)=><ProductShow {...props}/>}></Route>
-                <div style={{height:'10vh',width:'5vw',backgroundColor:'red'}} id='end'/>
-                <Box style={{height:'40vh'}}>
-    
-                </Box>
+                <Route exact path='/buy/:status' render={(props)=><Buy {...props}/>}></Route>
+                <div style={{height:'10vh',width:'100%',backgroundColor:'black'}} id='end'>
+                    <p className={classes1.boldFonts}>
+                    noteshare copyright
+                    </p>
+                    
+                </div>
             </div>
                    
         </Router>
